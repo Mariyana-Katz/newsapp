e6e74ad07af10642779c0d8ec76b31ad40be381e
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-05-04 13:06:05 -0400

    Created dropdown menu

--- src/main/webapp/app/modules/UserProfile/userProfile.tsx
+++ src/main/webapp/app/modules/UserProfile/userProfile.tsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import './userProfile.scss';
 import { Avatar } from 'primereact/avatar';
-import { Badge } from 'primereact/badge';
+import { useState } from 'react';
+import { ListBox } from 'primereact/listbox';
 
-export default function IconDemo() {
-  return (
-    <div className="card">
-      <div className="flex flex-wrap gap-5">
-        <div className="flex-auto">
-          <h5>Icon</h5>
-          <Avatar icon="pi pi-user" className="mr-2" size="xlarge" />
-          <Avatar icon="pi pi-user" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} />
-          <Avatar icon="pi pi-user" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} />
-        </div>
+export default function TemplateDemo() {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const countries = [
+    { name: 'Username', code: 'US' },
+    { name: 'Email', code: 'BR' },
+    { name: 'BookMark', code: 'US' },
+    { name: 'Logout', code: 'LS' },
+  ];
 
-        <div className="flex-auto">
-          <h5>Circle</h5>
-          <Avatar icon="pi pi-user" className="mr-2" size="xlarge" shape="circle" />
-          <Avatar icon="pi pi-user" className="mr-2" size="large" style={{ backgroundColor: '#2196F3', color: '#ffffff' }} shape="circle" />
-          <Avatar icon="pi pi-user" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" />
-        </div>
-
-        <div className="flex-auto">
-          <h5>Badge</h5>
-          <Avatar className="p-overlay-badge" icon="pi pi-user" size="xlarge">
-            <Badge value="4" />
-          </Avatar>
-        </div>
+  const countryTemplate = option => {
+    return (
+      <div className="flex align-items-center">
+        <img
+          alt={option.name}
+          src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
+          className={`flag flag-${option.code.toLowerCase()}`}
+          style={{ width: '1.25rem', marginRight: '.5rem' }}
+        />
+        <div>{option.name}</div>
       </div>
+    );
+  };
+
+  return (
+    <div className="card flex justify-content-center">
+      <ListBox
+        value={selectedCountry}
+        onChange={e => setSelectedCountry(e.value)}
+        options={countries}
+        optionLabel="name"
+        itemTemplate={countryTemplate}
+        className="w-full md:w-14rem"
+        listStyle={{ maxHeight: '250px' }}
+      />
     </div>
   );
 }
