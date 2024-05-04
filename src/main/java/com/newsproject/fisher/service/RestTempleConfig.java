28ba65e88b958b738cd503b2409173406e9391f1
andyzheng679 <andyzheng679@gmail.com>
2024-05-04 19:56:22 -0400

    adding multiple articles saving

--- src/main/java/com/newsproject/fisher/service/RestTempleConfig.java
+++ src/main/java/com/newsproject/fisher/service/RestTempleConfig.java
@@ -1,14 +1,20 @@
 package com.newsproject.fisher.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class Config {
+public class RestTempleConfig {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
--- src/main/java/com/newsproject/fisher/web/rest/ApiController.java
+++ src/main/java/com/newsproject/fisher/web/rest/ApiController.java
@@ -1,22 +1,33 @@
 package com.newsproject.fisher.web.rest;
 
 import com.newsproject.fisher.domain.Article;
-import com.newsproject.fisher.service.ApiService;
+import com.newsproject.fisher.repository.ArticleRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
     @Autowired
-    private ApiService apiService;
+    private ArticleRepository articleRepository;
 
-    @GetMapping("/testing")
-    public List<Article> mainPage() {
-        apiService.testingSave();
+    //takes in multiple articles
+    @PostMapping("/api/posting/multiple")
+    public ResponseEntity<List<Article>> savingDataFromApi(@RequestBody List<Article> articles) {
+        //@Requestbody uses Jackson library to deserialize the incoming JSON data into a list of Java Article objects.
+        List<Article> testArt = articleRepository.saveAll(articles);
+        return ResponseEntity.ok(testArt);
+    }
 
-        return apiService.getAllArticles();
+    //takes in a single article
+    @PostMapping("/api/posting")
+    public ResponseEntity<Article> savingDataFromApi(@RequestBody Article article) {
+        Article testArt = articleRepository.save(article);
+        return ResponseEntity.ok(testArt);
     }
 }
