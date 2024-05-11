460cd0d784f132cbcd033c5dd1561191f9fcd960
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-05-11 11:56:45 -0400

    finalized the logo

--- src/main/java/com/newsproject/fisher/web/rest/ApiController.java
+++ src/main/java/com/newsproject/fisher/web/rest/ApiController.java
@@ -102,7 +102,7 @@ public class ApiController {
         List<Article> savedArticles = articleRepository.saveAll(filteredArticles);
     }
 
-    //    @Scheduled(initialDelay = 60000, fixedRate = Long.MAX_VALUE)
+    // @Scheduled(initialDelay = 60000, fixedRate = Long.MAX_VALUE)
     //@Scheduled(cron = "0 0 6 * * *", zone = "America/New_York")
     public void cultureArticles() {
         List<Article> articles = apiService.fetchingCategoryData("culture");
