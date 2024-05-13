5d805a31866d6349b18bcc609f8cb79ddfb76e2d
Angelinamathew <angelinamathew707@gmail.com>
2024-05-13 08:45:40 -0400

    saving..

--- src/main/webapp/app/modules/articlecomponents/standardarticlebox.tsx
+++ src/main/webapp/app/modules/articlecomponents/standardarticlebox.tsx
@@ -27,14 +27,7 @@ const StandardArticleBox: React.FC = () => {
             <div className="article-card">
               <img src={article.urlToImage} alt="" className="article-image" />
               <div className="article-content">
-                <h3 className="article-headline">{article.title}</h3>
                 <p className="article-short-text">{article.shortDescription}</p>
-              </div>
-              {/* Pop-up container */}
-              <div className="article-details-popup">
-                {/* Content of the pop-up */}
-                {/* You can add more information or actions here */}
-                <p>Additional information about the article</p>
               </div>
             </div>
           </div>
