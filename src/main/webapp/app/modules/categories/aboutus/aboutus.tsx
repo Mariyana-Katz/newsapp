610fb0b7b0642a6724a9c76a30d24cda45ada398
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-05-15 00:01:26 -0400

    Added,aligned and positioned the images

--- src/main/webapp/app/modules/categories/aboutus/aboutus.tsx
+++ src/main/webapp/app/modules/categories/aboutus/aboutus.tsx
@@ -9,31 +9,53 @@ interface TeamMember {
 }
 
 const teamMembers: TeamMember[] = [
+  // https://photos.app.goo.gl/zqxoS11BNRD7Txxr9  https://photos.app.goo.gl/zqxoS11BNRD7Txxr9
   {
-    name: 'John Doe',
-    photoUrl: 'john-doe-photo.jpg',
-    qrCodeData: 'John Doe QR Code Data',
+    name: 'Mariyana Katzarova',
+    photoUrl: 'https://drive.google.com/thumbnail?id=1gU1XHrHOnoOoDQW4W2JqJsVQCLxYjQNR&sz=w5000',
+    qrCodeData: 'QR Code Data',
   },
   {
-    name: 'Jane Smith',
-    photoUrl: 'jane-smith-photo.jpg',
-    qrCodeData: 'Jane Smith QR Code Data',
+    name: 'Angelina Mathew',
+    photoUrl: 'https://drive.google.com/thumbnail?id=1FlHaXE7S5y5e2DiFacs_GMKStKVAs40c&sz=w5000',
+    qrCodeData: 'QR Code Data',
   },
-  // Add more team members as needed
+  {
+    name: 'Nicholas McNamara',
+    photoUrl: 'https://drive.google.com/thumbnail?id=1APP29rwgT5Bda1186vdVeBlqjTgtuSJ2&sz=w5000',
+    qrCodeData: 'QR Code Data',
+  },
+  {
+    name: 'Lavanya Palanikumar',
+    photoUrl: 'https://drive.google.com/thumbnail?id=1TK4OiP9CFXQ0wGyIL76JcXrvw6Tma-UQ&sz=w5000',
+    qrCodeData: 'QR Code Data',
+  },
+  {
+    name: 'Andy Zheng',
+    photoUrl: 'https://lh3.googleusercontent.com/d/17uJ_YQo_bBUIHy6iwX2_BD1lLu4ldPZl',
+    qrCodeData: 'QR Code Data',
+  },
 ];
 
 const AboutUsPage: React.FC = () => {
   return (
     <div>
       <h1>About Us</h1>
+      <p>
+        Orange Street Media is your vibrant online hub for the latest news, captivating stories, and insightful analyses across
+        entertainment, culture, technology, and beyond. From breaking headlines to in-depth features, we deliver engaging content that
+        sparks conversations and keeps you informed about the ever-evolving landscape of media and beyond.
+      </p>
+      <h2>Meet Our Team</h2>
       <div className="team-members">
         {teamMembers.map((member, index) => (
           <div key={index} className="team-member">
             <div className="member-info">
               <p className="name">{member.name}</p>
-              <img src={member.photoUrl} alt={member.name} className="rounded-photo" />
+              <img src={member.photoUrl} alt={member.name} className="photo" />
+              {/* <img src={require({member.photoUrl}).default} /> */}
             </div>
-            <QRCode value={member.qrCodeData} style={{ width: '64px', height: '64px', marginLeft: '20px' }} />
+            <QRCode value={member.qrCodeData} style={{ width: '150px', height: '15s0px', marginLeft: '100px' }} />
           </div>
         ))}
       </div>
